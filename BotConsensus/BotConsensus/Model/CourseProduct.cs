aab0ddbc0450befcf836ded50a314568fc3a7e00
ruchit-mehta <ruchit.mehta@oneadvanced.com>
2018-08-02 20:09:21 +0530

    Course models

---
+++ BotConsensus/BotConsensus/Model/CourseProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BotConsensus.Model
+{
+    public class CourseProduct
+    {
+        
+            public String Name { get; set; }
+
+            public string EntryDate { get; set; }
+
+            public string Id { get; set; }
+
+
+            public List<CourseProductPrice> PriceList { get; set; }
+
+            public double? CourseLength { get; set; }
+
+        
+    }
+}
---
+++ BotConsensus/BotConsensus/Model/CourseProductPrice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BotConsensus.Model
+{
+    public class CourseProductPrice
+    {
+        public String ListName { get; set; }
+
+        public Decimal UnitPrice { get; set; }
+    }
+}
