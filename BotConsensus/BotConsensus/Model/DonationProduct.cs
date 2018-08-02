2d9897f4380af6edbb2dfd87733b2ba7c2b784f3
Khyati <khyati.shah@oneadvanced.com>
2018-08-02 16:19:07 +0530

    Added DonationProduct class

---
+++ BotConsensus/BotConsensus/Model/DonationProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BotConsensus.Model
+{
+    public class DonationProduct
+    {
+        public String Name { get; set; }
+
+        public Double? Price { get; set; }
+
+        public string Id { get; set; }
+
+        public string PersonName { get; set; }
+
+        public string PersonSurname { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
